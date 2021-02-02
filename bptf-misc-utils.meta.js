f15515d082500f3cbcdd8f6fd77fa175c9d193a7
Bonfire <baranb@knights.ucf.edu>
2021-02-01 23:25:56 -0500

    fix: add downloadURL and updateURL

--- bptf-misc-utils.meta.js
+++ bptf-misc-utils.meta.js
@@ -5,6 +5,8 @@
 // @version      1.0.1
 // @description  A script to provide various backpack.tf miscellaneous utilities
 // @include      /^https?:\/\/backpack\.tf\/.*
+// @downloadURL  https://github.com/Bonfire/bptf-misc-utils/raw/master/bptf-misc-utils.user.js
+// @updateURL    https://github.com/Bonfire/bptf-misc-utils/raw/master/bptf-misc-utils.meta.js
 // @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
 // @run-at       document-end
 // ==/UserScript==
--- bptf-misc-utils.user.js
+++ bptf-misc-utils.user.js
@@ -5,11 +5,13 @@
 // @version      1.0.1
 // @description  A script to provide various backpack.tf miscellaneous utilities
 // @include      /^https?:\/\/backpack\.tf\/.*
+// @downloadURL  https://github.com/Bonfire/bptf-misc-utils/raw/master/bptf-misc-utils.user.js
+// @updateURL    https://github.com/Bonfire/bptf-misc-utils/raw/master/bptf-misc-utils.meta.js
 // @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
 // @run-at       document-end
 // ==/UserScript==
 
-(function () {
+https: (function () {
   "use strict";
 
   // Always make sure that the page is fully loaded
