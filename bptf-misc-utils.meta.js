ca43b56085c9e98c422b68e0b46ef28f9aacd09a
Bonfire <baranb@knights.ucf.edu>
2021-02-01 20:53:28 -0500

    feat: add meta userscript file

---
+++ bptf-misc-utils.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Backpack.tf - Misc Utils
+// @author       Bon
+// @namespace    https://github.com/Bonfire
+// @version      1.0.0
+// @description  A script to provide various backpack.tf miscellaneous utilities
+// @include      /^https?:\/\/backpack\.tf\/.*
+// @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @run-at       document-end
+// ==/UserScript==
